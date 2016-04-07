195392feb2bfe19454efd92f7f8592c747ff9e16
Antoine Maleyrie <am@atim.com>
2016-04-07 18:08:17 +0200

    updtae makepkg for all pkg

--- tool/pkg_arduino/makepkg.sh
+++ tool/pkg_arduino/makepkg.sh
@@ -2,16 +2,38 @@
 
 ########################################################################
 #
-#	Script to prepare Arduino package library.
-# - date : 07.02.2016
+# - Script to prepare package library.
+# - Date : 07.02.2016
 #
 ########################################################################
 
-#Prepare pkg
+libVersion=$1
+
+if [ -z "$libVersion" ]; then
+	echo "What is the library version?"
+	exit
+fi
+
+########################################################################
+# Arduino pkg
+########################################################################
+
+##Prepare pkg for arduino
 mkdir -p armapi/src/
 mkdir -p armapi/exemples/
 
-cp library.properties armapi/
+#cp library.properties armapi/
+#Cerate library.properties file
+echo "name=armapi" > armapi/library.properties
+echo "version=$libVersion" >> armapi/library.properties
+echo "author=Atim" >> armapi/library.properties
+echo "maintainer=Atim" >> armapi/library.properties
+echo "sentence=This API allow you to control easily the modules ARM from ATIM company." >> armapi/library.properties
+echo "paragraph=You will be able to send and receive data on the Sigfox/LoRaWan network and local(fsk) network." >> armapi/library.properties
+echo "category=Communication" >> armapi/library.properties
+echo "url=http://atim-radiocommunications.github.io/armapi/" >> armapi/library.properties
+echo "architectures=*" >> armapi/library.properties
+
 cp ../../LICENSE armapi/
 cp ../../README.md armapi/
 
@@ -25,7 +47,7 @@ cp ../../bindings/cpp/arm.cpp armapi/src/
 cp ../../bindings/cpp/armport/armport_arduino.cpp armapi/src/
 
 #Make pkg
-zip -r armapi.zip armapi
+zip -r arduino_armapi_$libVersion.zip armapi
 
 #Install the pkg
 rm -fr ~/Arduino/libraries/armapi/
@@ -33,3 +55,44 @@ cp -r armapi ~/Arduino/libraries/armapi/
 
 #Clean
 rm -rf armapi
+
+echo "Pkg for Arduino generated."
+
+########################################################################
+# cpp pkg
+########################################################################
+
+##Prepare pkg for arduino
+mkdir -p armapi/src/
+
+cp ../../LICENSE armapi/
+cp ../../README.md armapi/
+
+cp -r ../../bindings/cpp/* armapi/src/
+
+#Make pkg
+zip -r cpp_armapi_$libVersion.zip armapi
+
+#Clean
+rm -rf armapi
+
+echo "Pkg for cpp generated."
+########################################################################
+# c pkg
+########################################################################
+
+##Prepare pkg for arduino
+mkdir -p armapi/src/
+
+cp ../../LICENSE armapi/
+cp ../../README.md armapi/
+
+cp -r ../../bindings/c/* armapi/src/
+
+#Make pkg
+zip -r c_armapi_$libVersion.zip armapi
+
+#Clean
+rm -rf armapi
+
+echo "Pkg for c generated."
